fc49915f252dccf4970882671c691c070fb842e9
Neenick <y.j.van.maanen@student.rug.nl>
2024-11-01 22:40:51 +0100

    Made Model ABC with abstractmethods fit and predict

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -1,10 +1,15 @@
-
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from autoop.core.ml.artifact import Artifact
 import numpy as np
 from copy import deepcopy
 from typing import Literal
 
-class Model:
-    pass # your code (attribute and methods) here
-    
+class Model(ABC):
+    # your code (attribute and methods) here
+    @abstractmethod
+    def fit(self, X, Y):
+        pass
+
+    @abstractmethod
+    def predict(self, X):
+        pass
