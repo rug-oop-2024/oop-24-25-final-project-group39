e5d32d262c54d5a67e44f70987670fcc023cda6b
Neenick <y.j.van.maanen@student.rug.nl>
2024-11-03 00:50:17 +0100

    Finished last two metrics

--- autoop/core/ml/metric.py
+++ autoop/core/ml/metric.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Any, Union
+from typing import Any, Union, Callable
 import numpy as np
-from typing import Callable
 from math import sqrt
 
 METRICS = [
@@ -13,8 +12,9 @@ METRICS = [
     "recall"
 ]
 
-def get_metric(name: str) -> Union["MeanAbsoluteError", "MeanSquaredError", 
-                                   "RootMeanSquaredError", "Accuracy", 
+
+def get_metric(name: str) -> Union["MeanAbsoluteError", "MeanSquaredError",
+                                   "RootMeanSquaredError", "Accuracy",
                                    "Precision", "Recall"]:
     # Factory function to get a metric by name.
     # Return a metric instance given its str name.
@@ -34,42 +34,51 @@ def get_metric(name: str) -> Union["MeanAbsoluteError", "MeanSquaredError",
         case "recall":
             return Recall()
 
+
 class Metric(ABC):
     """
     Base class for all metrics.
     """
     # your code here
-    # remember: metrics take ground truth and prediction as input and return a real number
+    # remember: metrics take ground truth and prediction
+    # as input and return a real number
     @abstractmethod
-    def evaluate(y_true, y_pred):
+    def evaluate(y_true: np.ndarray, y_pred: np.ndarray) -> float:
         pass
 
     def __call__(self) -> Callable[[any], any]:
-        return self.evaluate() # Not sure about this
-    
+        return self.evaluate()  # Not sure about this
+
 
 class MeanAbsoluteError(Metric):
-    def evaluate(self, y_true, y_pred) -> float:
+    def evaluate(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         return np.mean(np.abs(y_true - y_pred))
 
+
 class MeanSquaredError(Metric):
-    def evaluate(self, y_true, y_pred) -> float:
+    def evaluate(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         return np.mean((y_true - y_pred) ** 2)
 
+
 class RootMeanSquaredError(Metric):
-    def evaluate(self, y_true, y_pred) -> float:
-        # Maybe change this later to the next code if it works:
-        # return sqrt(MeanSquaredError.evaluate(y_true-y_pred))
+    def evaluate(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         return sqrt(np.mean((y_true - y_pred) ** 2))
 
+
 class Accuracy(Metric):
-    def evaluate(self, y_true, y_pred):
+    def evaluate(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         return np.mean(y_true == y_pred)
 
-class Precision(Metric):
-    def evaluate(self, y_true, y_pred):
-        pass
+
+class Precision(Metric): # Precision = TruePositive/ (Truepositive + FalsePositive)
+    def evaluate(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        true_positive = np.sum((y_true == 1) & (y_pred == 1))
+        false_positive = np.sum((y_true == 0) & (y_pred == 0))
+        return true_positive / (true_positive + false_positive)
+
 
 class Recall(Metric):
-    def evaluate(self, y_true, y_pred):
-        pass
+    def evaluate(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        true_positive = np.sum((y_true == 1) & (y_pred == 1))
+        false_negative = np.sum((y_true == 0) & (y_pred == 1))
+        return true_positive / (true_positive + false_negative)
