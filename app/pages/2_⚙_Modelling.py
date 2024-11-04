43a13831be989c5372309fd50791b77d56d670f6
Neenick <y.j.van.maanen@student.rug.nl>
2024-11-04 21:32:22 +0100

    Created choosing dataset and features

--- app/pages/2_⚙_Modelling.py
+++ app/pages/2_⚙_Modelling.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 from app.core.system import AutoMLSystem
 from autoop.core.ml.dataset import Dataset
+from io import BytesIO
+from autoop.functional.feature import detect_feature_types
 
 
 st.set_page_config(page_title="Modelling", page_icon="📈")
@@ -18,4 +20,31 @@ automl = AutoMLSystem.get_instance()
 datasets = automl.registry.list(type="dataset")
 
 # your code here
+
+
+dataset = None
+st.header("Step 1. Choose Dataset")
+if len(datasets) == 0:
+    st.write("There are no saved datasets. Please go to the Datasets page to save datasets.")
+else:
+    dataset = st.selectbox("Select dataset:", datasets)
+    dataset_to_csv = BytesIO(dataset.data)
+    df = pd.read_csv(dataset_to_csv)
+    st.write(df.head())
+
+st.header("Step 2. Select Features")
+
+if dataset is None:
+    st.write("Please select a dataset first.")
+else:
+    feature_list = detect_feature_types(dataset)
+    input_features = st.multiselect("Select input features", feature_list)
+    input_feature_names = [feature.name for feature in input_features]
+    target_feature = st.selectbox("Select target feature", feature_list)
+    if input_features is not None and target_feature is not None:
+        if target_feature.name in input_feature_names:
+            st.write("Target feature can not be one of the input features.")
+        else:
+            st.write(f"Input features: {', '.join([feature.name for feature in input_features])}")
+            st.write(f"Target feature: '{target_feature.name}'")
 
